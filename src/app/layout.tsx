c3013da82657a2daf7af37ce82f426f0074109a7
Efaliso Tamerat <efalisotamerat@gmail.com>
2024-07-31 10:37:11 +0200

    Initial scaffold init

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "analytics-dashboard",
+  description: "$A website for analytics dashboards with metrics and information about StaffingHQ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
